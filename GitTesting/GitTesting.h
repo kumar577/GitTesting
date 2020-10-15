73ed5eadb33110aa70f56cf4c6eee2b891dc02f1
kumarutsav7 <kumar.u@vectone.com>
2020-10-16 02:34:01 +0530

    Initial Commit

---
+++ GitTesting/GitTesting.h
@@ -0,0 +1,18 @@
+//
+//  GitTesting.h
+//  GitTesting
+//
+//  Created by VEENA on 16/10/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GitTesting.
+FOUNDATION_EXPORT double GitTestingVersionNumber;
+
+//! Project version string for GitTesting.
+FOUNDATION_EXPORT const unsigned char GitTestingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GitTesting/PublicHeader.h>
+
+
