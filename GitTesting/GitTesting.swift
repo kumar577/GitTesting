f0792850c99f5e86742777a72d152f45734dcd1c
kumarutsav7 <avecutsav111@gmail.com>
2020-10-16 10:54:22 +0530

    Git file and testing file

---
+++ GitTesting/GitTesting.swift
@@ -0,0 +1,21 @@
+//
+//  GitTesting.swift
+//  GitTesting
+//
+//  Created by VEENA on 16/10/20.
+//
+
+import Foundation
+
+struct GitTesting {
+  var gitPractise: Int
+  func testgitbranching() -> Int {
+    return 120
+  }
+  func eachBranchHasUniqueColor() -> Bool {
+    return true
+  }
+  func doRebase() -> Bool {
+    return true
+  }
+}
--- GitTestingTests/GitTestingTests.swift
+++ GitTestingTests/GitTestingTests.swift
@@ -10,24 +10,29 @@ import XCTest
 
 class GitTestingTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override class func setUp() {
+        print(" i just run")
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override class func tearDown() {
+        print("oh I am useless")
     }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let sut = GitTesting(gitPractise: 120)
+        XCTAssertTrue(sut.testgitbranching() == 120, "they match")
+
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testeachBranchHasUniqueColor() {
+        let sut = GitTesting(gitPractise: 120)
+        XCTAssertTrue(sut.eachBranchHasUniqueColor() == true, "It has to be true")
+
     }
+
+  func testdoRebase() {
+    let sut = GitTesting(gitPractise: 120)
+    XCTAssertTrue(sut.doRebase() == true, " rebase is good")
+
+  }
 
 }
